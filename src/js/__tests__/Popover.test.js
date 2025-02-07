f7fae90909c452a5a83bb56d83040c86d25225f6
Angry Baker <checheninlive@mail.ru>
2025-02-07 18:25:03 +0300

    update tests

--- src/js/__tests__/Popover.test.js
+++ src/js/__tests__/Popover.test.js
@@ -8,12 +8,34 @@ test("popover", () => {
   document.body.innerHTML = `<button class="btn"  title="Внимание" data-content="Вы нажали на большую красную кнопку">Click to toggle popover</button>`;
   const popoverFactory = new Popover();
   const btn = document.querySelector(".btn");
-  popoverFactory.showPopover(btn);
+  popoverFactory.addHandler(btn);
+  btn.click();
 
   setTimeout(() => {
     const popoverTitle = document.querySelector(".popover-title");
     expect(popoverTitle.innerText).toEqual("Внимание");
     // eslint-disable-next-line no-undef
     done(); // Сообщаем Jest, что тест завершён
+  }, 0);
+});
+
+test("popover should be closed", () => {
+  document.body.innerHTML = `<button class="btn"  title="Внимание" data-content="Вы нажали на большую красную кнопку">Click to toggle popover</button>`;
+  const popoverFactory = new Popover();
+  const btn = document.querySelector(".btn");
+  popoverFactory.addHandler(btn);
+  btn.click();
+
+  setTimeout(() => {
+    expect(document.querySelector(".popover-title")).not.toBeNull();
+
+    // Второй клик - скрыть поповер
+    btn.click();
+
+    setTimeout(() => {
+      expect(document.querySelector(".popover-title")).toBeNull();
+      // eslint-disable-next-line no-undef
+      done();
+    }, 0);
   }, 0);
 });
